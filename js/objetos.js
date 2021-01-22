dc64549f2492e700fc9430dce02767a042c4a698
Sknor <geafalconjoseantonio@gmail.com>
2021-01-22 04:56:27 +0100

    repaso javascript

---
+++ js/objetos.js
@@ -0,0 +1,11 @@
+// Objeto literal
+
+
+const persona = {
+    nombre: "Jose",
+    profesion: "Desarrollador",
+    edad: 26
+}
+
+
+console.log(persona)
---
+++ js/scope.js
@@ -0,0 +1,35 @@
+// SCOPE
+
+// var musica = "rock"
+
+// if (musica) {
+//     var musica = "gruge"
+//     console.log("Dentro del if ", musica)
+// }
+// console.log("fuera del if ", musica)
+
+// scope con let 
+// var musica = "rock"
+
+// if (musica) {
+//     let musica = "gruge"
+//     console.log("Dentro del if ", musica)
+// }
+// console.log("fuera del if ", musica)
+
+// template strings 
+
+// const nombre = " Jose "
+// let trabajo = "Desarrollador web"
+
+// concatenar javascript
+
+// console.log("Nombre:", nombre, "trabajo", trabajo)
+// console.log(`nombre: ${nombre}, trabajo, ${trabajo}`)
+
+// concatenar multiples lineas  
+
+// const contenedorApp = document.querySelector('#app')
+// let html = `<ul>  <li> nombre  ${nombre}  </li>  <li>trabajo  ${trabajo}  </li>  </ul>`;
+
+// contenedorApp.innerHTML = html
