d5976b6dbcfad79ead1bf76d487d37972cae5b04
Sknor <geafalconjoseantonio@gmail.com>
2021-01-26 01:57:20 +0100

    objeto constructor

--- js/objetos.js
+++ js/objetos.js
@@ -9,3 +9,20 @@ const persona = {
 
 
 console.log(persona)
+
+// Object Constructor
+
+function Tarea(nombre, urgencia) {
+    this.nombre = nombre
+    this.urgencia = urgencia
+}
+
+// crear nueva tarea  
+
+const tarea1 = new Tarea("aprender Javascript", "Urgente")
+const tarea2 = new Tarea("Sacar la basura", "media")
+const tarea3 = new Tarea("Hablar con la abogada", "Urgente")
+
+console.log(tarea1)
+console.log(tarea2)
+console.log(tarea3)
